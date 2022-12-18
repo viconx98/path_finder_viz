6b828584b174c904723c0ca439f813f93787ddee
Vihar <vihar1998x@gmail.com>
2022-12-18 12:52:36 +0530

    WIP: Running algorithm loop with thunks

--- src/slices/visualizerSlice.ts
+++ src/slices/visualizerSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+import { AppAsyncThunkConfig, RootState } from '../store'
 import { generateMaze, generateVisitedMaze, isValidCell, OFFSETS_SIMPLE } from '../utils'
 
 interface VisualizerState {
@@ -38,6 +39,70 @@ const initialState: VisualizerState = {
     algorithm: "dfs",
     algorithmStatus: "stopped"
 }
+
+// TODO: Better typing
+const setupAlgorithmLoopAsync = createAsyncThunk<
+    boolean,
+    void,
+    { state: RootState }
+>(
+    "visualizerSlice/setupAlgorithmLoopAsync",
+    async (_, { getState, dispatch: AppDispatch }) => {
+        return true
+    }
+)
+
+const runAlgorithmLoopAsync = createAsyncThunk<
+    boolean,
+    void,
+    AppAsyncThunkConfig
+>(
+    "visualizerSlice/runAlgorithmLoopAsync",
+    async (_, { getState, dispatch: AppDispatch, rejectWithValue, fulfillWithValue }) => {
+        // Guard statement to prevent redundant actions from setInterval
+        if (getState().visualizer.algorithmStatus === "completed") {
+            return rejectWithValue(false)
+        }
+
+        const currentCell = getState().visualizer.algorithm === "bfs"
+            ? getState().visualizer.array.shift()
+            : getState().visualizer.array.pop()
+
+        // Array is empty and there are no more cells to search, meaning no path was found
+        if (currentCell === undefined) {
+            getState().visualizer.algorithmStatus = "completed"
+            return rejectWithValue(false)
+        }
+
+        const [currentRow, currentCol, pathSoFar] = currentCell
+
+        getState().visualizer.visitedMaze[currentRow][currentCol] = true
+
+        // Path from start to finish was found
+        if (currentRow === getState().visualizer.finishRowIdx
+            && currentCol === getState().visualizer.finishColIdx) {
+            getState().visualizer.algorithmStatus = "completed"
+            getState().visualizer.validPath = pathSoFar
+            return fulfillWithValue(true)
+        }
+
+        // Push next possible adjacent cells into the array
+        for (const [offsetX, offsetY] of OFFSETS_SIMPLE) {
+            const nextRow = currentRow + offsetX
+            const nextCol = currentCol + offsetY
+
+            if (isValidCell(getState().visualizer.mazeWidth, getState().visualizer.mazeHeight, nextRow, nextCol)
+                && !getState().visualizer.visitedMaze[nextRow][nextCol]
+                && getState().visualizer.maze[nextRow][nextCol].state !== "block") {
+
+                getState().visualizer.array.push([nextRow, nextCol, [...pathSoFar, [nextRow, nextCol]]])
+                getState().visualizer.visitedMaze[nextRow][nextCol] = true
+            }
+        }
+
+        return rejectWithValue(false)
+    }
+)
 
 export const visualizerSlice = createSlice({
     name: 'visualizer',
@@ -83,7 +148,7 @@ export const visualizerSlice = createSlice({
             if (state.algorithmStatus === "completed") {
                 return
             }
-            
+
             const currentCell = state.algorithm === "bfs" ? state.array.shift() : state.array.pop()
 
             // Array is empty and there are no more cells to search, meaning no path was found
@@ -117,7 +182,27 @@ export const visualizerSlice = createSlice({
                 }
             }
         }
-    }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(setupAlgorithmLoopAsync.pending, (state, action) => {
+
+        }).addCase(setupAlgorithmLoopAsync.fulfilled, (state, action) => {
+            state.array.push([state.startRowIdx, state.startColIdx, [[state.startRowIdx, state.startColIdx]]])
+            state.visitedMaze[state.startRowIdx][state.startColIdx] = true
+
+            state.algorithmStatus = "running"
+        }).addCase(setupAlgorithmLoopAsync.rejected, (state, action) => {
+
+        })
+
+        builder.addCase(runAlgorithmLoopAsync.pending, (state, action) => {
+
+        }).addCase(runAlgorithmLoopAsync.fulfilled, (state, action) => {
+            console.log(state.algorithm)
+        }).addCase(runAlgorithmLoopAsync.rejected, (state, action) => {
+
+        })
+    },
 })
 
 export const { initializeMaze, toggleBlockCell, setVisitedCell, setupAlgorithmLoop, runAlgorithmLoop } = visualizerSlice.actions
